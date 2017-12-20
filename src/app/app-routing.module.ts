7369e5cc5a66dd2deec344906cc15ce67636fb2d
Lohmann <Enno.Lohmann@adesso.de>
2017-12-20 13:37:58 +0100

    Add stub for Dashboard Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'editor', loadChildren: './hero-editor/hero-editor.module#HeroEditorModule'},
   {path: 'list', loadChildren: './hero-list/hero-list.module#HeroListModule'},
-  {path: 'creation', loadChildren: './hero-creation/hero-creation.module#HeroCreationModule'}
+  {path: 'creation', loadChildren: './hero-creation/hero-creation.module#HeroCreationModule'},
+  {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule'}
 ];
 
 @NgModule({
