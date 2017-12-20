17d29b0fcaf451eda51c31028c2d653f5a6b71c3
Lohmann <Enno.Lohmann@adesso.de>
2017-12-20 13:23:14 +0100

    Add Stub for HeroEditorModule

---
+++ src/app/hero-editor/hero-editor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroEditorRoutingModule } from './hero-editor-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    HeroEditorRoutingModule
+  ],
+  declarations: []
+})
+export class HeroEditorModule { }
