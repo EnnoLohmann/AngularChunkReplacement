4498ae116a33ad2136f8cc5b49813b14b3c6aa97
Lohmann <Enno.Lohmann@adesso.de>
2017-12-20 13:24:08 +0100

    Add shared Module to stub

--- src/app/hero-editor/hero-editor.module.ts
+++ src/app/hero-editor/hero-editor.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroEditorRoutingModule } from './hero-editor-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    HeroEditorRoutingModule
+    HeroEditorRoutingModule,
+    SharedModule
   ],
   declarations: []
 })
