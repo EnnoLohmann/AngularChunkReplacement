23a9b38fd65d2c40a1b970b13ede52d372e0ab19
Lohmann <Enno.Lohmann@adesso.de>
2017-12-21 10:20:52 +0100

    Example use of Library

--- src/app/hero-editor/editor/editor.component.ts
+++ src/app/hero-editor/editor/editor.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef  } from '@angular/core';
 import { Hero } from '../../shared/Domain/Hero';
 import { HeroService } from '../../shared/services/hero.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-editor',
@@ -10,11 +12,16 @@ import { HeroService } from '../../shared/services/hero.service';
 export class EditorComponent implements OnInit {
 
   hero;
- 
-  constructor(private heroService: HeroService) { }
+  modalRef: BsModalRef;
+  
+  constructor(private heroService: HeroService, private modalService: BsModalService) { }
  
   ngOnInit() {
     this.hero = this.heroService.getSelectedHero();
   }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  
  
 }
