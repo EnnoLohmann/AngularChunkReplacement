f9f5f037098ebf0f687c838c7cea9fe5c871a80a
Lohmann <Enno.Lohmann@adesso.de>
2017-12-21 10:08:05 +0100

    Add Hero Creation Fomular

--- src/app/hero-creation/hero-creation.module.ts
+++ src/app/hero-creation/hero-creation.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroCreationRoutingModule } from './hero-creation.routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { CreationComponent } from './creation/creation.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,8 @@ import { SharedModule } from '../shared/shared.module';
     HeroCreationRoutingModule,
     SharedModule
   ],
-  declarations: []
+  declarations: [
+    CreationComponent
+  ]
 })
 export class HeroCreationModule { }
--- src/app/hero-creation/hero-creation.routing.module.ts
+++ src/app/hero-creation/hero-creation.routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreationComponent } from './creation/creation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CreationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
