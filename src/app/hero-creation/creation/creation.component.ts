1fe00dbcf570e3ea5dc3d79d8ffceec62a2b5ed2
Lohmann <Enno.Lohmann@adesso.de>
2017-12-21 12:58:29 +0100

    Add Graph to hero creation

--- src/app/hero-creation/creation/creation.component.ts
+++ src/app/hero-creation/creation/creation.component.ts
@@ -11,6 +11,20 @@ export class CreationComponent implements OnInit {
 
   newHero: Hero = new Hero();
 
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+ 
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+ 
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -18,6 +32,37 @@ export class CreationComponent implements OnInit {
 
   createHero(){
     this.heroService.addHero(this.newHero);
+  }
+
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
+ 
+  public randomize():void {
+    // Only Change 3 values
+    let data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40];
+    let clone = JSON.parse(JSON.stringify(this.barChartData));
+    clone[0].data = data;
+    this.barChartData = clone;
+    /**
+     * (My guess), for Angular to recognize the change in the dataset
+     * it has to change the dataset variable directly,
+     * so one way around it, is to clone the data, change it and then
+     * assign it;
+     */
   }
 
 }
